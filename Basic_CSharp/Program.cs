0b05edf3024e59e50ccc86c61be619d01d6daf24
dhumildholiya-code <dhumildholiya97@gmail.com>
2022-04-04 16:21:15 +0530

    Initial Commit

---
+++ Basic_CSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World! Dhumil");
+
+// We want to write a program which adds two numbers.
+// <data-type> <variable name> = <value>
+// variable Name : shouldnot start with number. should not start with wild card characters (`, ~, .- +)
+
+int a = 2;
+int b = 3; 
+
+// arimatica operations such as  +, -, /, * , %
+
+int c = a + b; 
+
+c = a - b;
+
+Console.WriteLine(c);
+
+float d = 1.5f;
+float e = 5f; // 5.000000000
+
+Console.WriteLine(d + e);
+
+//Logical operators. (&& , || , == ,>, <, >=, <=)
+bool f1 = true;
+bool f2 = false;
+
+char c1 = 'a';
+char c2 = 'b';
+
+Console.WriteLine(c1);
+
+string s1 = "Dhumil";
+string s2 = "Katrina";
+
+Console.WriteLine(s1 + " -> " + s2  + a);
