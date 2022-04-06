5d163a59f0295465bfe42a89aa773ded65f12353
dhumildholiya-code <dhumildholiya97@gmail.com>
2022-04-06 09:37:51 +0530

    read line program

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -1,19 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string s = Console.ReadLine();
 
-//type casting
-// implicit type casting
-// char -> int -> long -> float -> double
-
-// explicit type casting
-// double -> float -> long -> int -> char
-
-int b = 3;
-
-int a = 'a';
-float c = 'a';
-
-char d = (char) 5.5;
-
-Console.WriteLine(d);
+Console.WriteLine("Hello World " + s);
