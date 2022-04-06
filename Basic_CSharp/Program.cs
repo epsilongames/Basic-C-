e607835741eb00b496a863981dfeb93647f73a49
dhumildholiya-code <dhumildholiya97@gmail.com>
2022-04-06 10:32:22 +0530

    basic of opertators.

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -1,5 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string s = Console.ReadLine();
+Console.WriteLine("Hello World!");
 
-Console.WriteLine("Hello World " + s);
+// Operators
+// Arithmatic operator -> +, -, /, *, %, ++, --
+// Assignment operator -> =, += , -=, *=, /=
+// Comparision operator -> >, < ,>=, <= , ==
+// Logical operator -> &&, ||, !
+
+int a = 1;
+int b = 2;
+
+//a = a + 1;
+//b++;
+
+//a = a - 1;
+//b--;
+
+a += 5; // a = a + 5;
+b *= 2; // b = b * 2;
+
+bool x = a <= b;
+
+a = 1;
+b = 1;
+
+bool y = a == b;
+
+bool and = x && y;
+bool or = x || y;
+bool not = !and;
+
+Console.WriteLine("X : " + x);
+Console.WriteLine("y : " + y);
+Console.WriteLine("and : " + and);
+Console.WriteLine("or: " + or);
+Console.WriteLine("not : " + not);
